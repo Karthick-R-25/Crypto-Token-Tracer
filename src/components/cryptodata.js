95b3f43e7ec86aee20db9a7279b72e6ccaa1300d
Karthick-R-25 <karthickr2429@gmail.com>
2025-04-24 18:09:55 +0530

    image are changed to assets

--- src/components/cryptodata.js
+++ src/components/cryptodata.js
@@ -1,68 +1,83 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 
+// ✅ Import assets from the assets folder
+import yellowStar from '../assets/yellow-star.png';
+import star from '../assets/star.png';
+import increasedArrow from '../assets/increasedarrow.png';
+import decreasedArrow from '../assets/decreased-arrow.png';
 
 const PercentCell = ({ value }) => {
-    const isPositive = value > 0;
-    const isNegative = value <= 0;
-  
-    return (
-      <td className={`decreaser ${isPositive ? 'text-green' : isNegative ? 'text-red' : 'text-gray'}`}>
-        {isPositive && <img className="increaser" src="/increasedarrow.png" alt="up"  />}
-        {isNegative && <img className="increaser" src="/decreased-arrow.png" alt="down" />}
-        {value}%
-      </td>
-    );
-  };
-  
-  
-  const Bar = ({ percent }) => (
-    <div className="bar">
-      <div className="fill" style={{ width: `${percent}%` }} />
-    </div>
+  const isPositive = value > 0;
+  const isNegative = value <= 0;
+
+  return (
+    <td className={`decreaser ${isPositive ? 'text-green' : isNegative ? 'text-red' : 'text-gray'}`}>
+      {isPositive && <img className="increaser" src={increasedArrow} alt="up" />}
+      {isNegative && <img className="increaser" src={decreasedArrow} alt="down" />}
+      {value}%
+    </td>
   );
-  
-  
-  
-  
-  const SupplyCell = ({ value, percent, token }) => (
-      <div>
-      <div>{value} {token}</div>
-      <Bar percent={percent} /></div>
- 
-  );
-  
+};
+
+const Bar = ({ percent }) => (
+  <div className="bar">
+    <div className="fill" style={{ width: `${percent}%` }} />
+  </div>
+);
+
+const SupplyCell = ({ value, percent, token }) => (
+  <div>
+    <div>{value} {token}</div>
+    <Bar percent={percent} />
+  </div>
+);
 
 const CryptoRow = ({ asset, index }) => {
-    const [isFavourite, setIsFavourite] = useState(false);
+  const [isFavourite, setIsFavourite] = useState(false);
 
   const toggleFavourite = () => {
     setIsFavourite(prev => !prev);
   };
-  return(
-  <tr className="border-b" onClick={toggleFavourite} >
-    <td><img
-          src={isFavourite ? '/yellow-star.png' : '/star.png'}
+
+  return (
+    <tr className="border-b" onClick={toggleFavourite}>
+      <td>
+        <img
+          src={isFavourite ? yellowStar : star}
           className='star'
           alt='icon not found'
-        /></td>
-    <td>{index}</td>
-    <td ><div className='symbols'><img className='crypto-logo' src={asset.symbol} alt="logo not found" /><p>{asset.name}</p><p>{asset.short}</p></div></td>
-   
-    <td>${asset.price.toLocaleString()}</td>
-    <PercentCell value={asset.change1h} />
-    <PercentCell value={asset.change24h} />
-    <PercentCell value={asset.change7d} />
-    <td>${asset.marketCap.toLocaleString()}</td>
-    <td ><div className='volume-10'><div className='volumep'>${asset.volume24h.toLocaleString()}</div><div className='volumetoken'>{asset.volumeToken.toLocaleString()}{asset.short}</div></div></td>
-    <td >
-     <div className="volume-1">
-    <div className='supply3'>{asset.supply}   {asset.short}</div>
-    <SupplyCell percent={asset.supplyPercent} /></div>
-   </td>
-
-    <td><img src={asset.chart} alt="chart" className="chart" /></td>
-
-  </tr>
-)};
+        />
+      </td>
+      <td>{index}</td>
+      <td>
+        <div className='symbols'>
+          <img className='crypto-logo' src={asset.symbol} alt="logo not found" />
+          <p>{asset.name}</p>
+          <p>{asset.short}</p>
+        </div>
+      </td>
+      <td>${asset.price.toLocaleString()}</td>
+      <PercentCell value={asset.change1h} />
+      <PercentCell value={asset.change24h} />
+      <PercentCell value={asset.change7d} />
+      <td>${asset.marketCap.toLocaleString()}</td>
+      <td>
+        <div className='volume-10'>
+          <div className='volumep'>${asset.volume24h.toLocaleString()}</div>
+          <div className='volumetoken'>{asset.volumeToken.toLocaleString()}{asset.short}</div>
+        </div>
+      </td>
+      <td>
+        <div className="volume-1">
+          <div className='supply3'>{asset.supply} {asset.short}</div>
+          <SupplyCell percent={asset.supplyPercent} />
+        </div>
+      </td>
+      <td>
+        <img src={asset.chart} alt="chart" className="chart" />
+      </td>
+    </tr>
+  );
+};
 
 export default CryptoRow;
--- src/components/cryptoheader.js
+++ src/components/cryptoheader.js
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { updateRandomFields } from '../slice/cryptoSlice.js';
 import CryptoRow from './cryptodata';
 
+// ✅ Import i-icon from assets
+import infoIcon from '../assets/i-icon.png';
 
 const CryptoTable = () => {
   const dispatch = useDispatch();
@@ -15,7 +17,6 @@ const CryptoTable = () => {
 
     return () => clearInterval(interval);
   }, [dispatch]);
-  
 
   return (
     <table className="w-full text-sm text-left">
@@ -24,14 +25,31 @@ const CryptoTable = () => {
           <th></th>
           <th>#</th><th>Name</th><th>Price</th>
           <th>1h %</th><th>24h %</th><th>7d %</th>
-          <th><div className='i-head'><p>Market Cap</p><img  className='i-icon' src='/i-icon.png' alt='information'/></div></th>
-          <th><div className='i-head'><p>Volume (24h)</p><img  className='i-icon' src='/i-icon.png' alt='information'/></div></th>
-          <th><div className='i-head'><p>Circulating Supply</p><img  className='i-icon' src='/i-icon.png' alt='information'/></div></th>
+          <th>
+            <div className='i-head'>
+              <p>Market Cap</p>
+              <img className='i-icon' src={infoIcon} alt='information' />
+            </div>
+          </th>
+          <th>
+            <div className='i-head'>
+              <p>Volume (24h)</p>
+              <img className='i-icon' src={infoIcon} alt='information' />
+            </div>
+          </th>
+          <th>
+            <div className='i-head'>
+              <p>Circulating Supply</p>
+              <img className='i-icon' src={infoIcon} alt='information' />
+            </div>
+          </th>
           <th>Last 7 Days</th>
         </tr>
       </thead>
       <tbody>
-        {data.map((asset, index) => <CryptoRow key={asset.id} asset={asset} index={index + 1} />)}
+        {data.map((asset, index) => (
+          <CryptoRow key={asset.id} asset={asset} index={index + 1} />
+        ))}
       </tbody>
     </table>
   );
